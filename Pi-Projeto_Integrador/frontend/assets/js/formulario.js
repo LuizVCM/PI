530629a7211a5f8c149ea540cb68d5056adbe3ce
LuizVCM <luizvcmluizvcm@gmail.com>
2026-05-12 17:15:18 -0300

    feat: adiciona o arquivo incompleto home.html, com o seu próprio css e js

--- Pi-Projeto_Integrador/frontend/assets/js/formulario.js
+++ Pi-Projeto_Integrador/frontend/assets/js/formulario.js
@@ -1,6 +1,7 @@
 const formulario = document.querySelectorAll(".fundo");
 const click = document.getElementById("paragrafoLink");
-const click2 = document.getElementById("entra2")
+const click2 = document.getElementById("entra2");
+const entrar = document.getElementById("find");
 
 click.addEventListener("click", () => {
     formulario.forEach(element => {
@@ -11,4 +12,5 @@ click2.addEventListener("click", () => {
     formulario.forEach(element => {
         element.classList.toggle("cadastro");
     });
-})
+});
+
