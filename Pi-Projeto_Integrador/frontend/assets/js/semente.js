64389e183da69c9782bdfc4993cd90d7820ac2c7
LuizVCM <luizvcmluizvcm@gmail.com>
2026-06-18 17:17:05 -0300

    feat: adiciona mais progresso

---
+++ Pi-Projeto_Integrador/frontend/assets/js/semente.js
@@ -0,0 +1,12 @@
+const content = document.querySelector(".content");
+const validade = document.querySelector(".validade")
+const entrarValidade = document.querySelector(".funcao1");
+entrarValidade.addEventListener("click", () => {
+    content.classList.toggle("toggle1");
+    validade.classList.toggle("valMovimento")
+
+});
+
+// funções de 'nova semente'
+const novaSemente = document.querySelector(".novaSemente");
+
