a5a85312e1d66dbf832fdbf37f1cb73949c8bbe2
Eduarda C. <dossantosec@outlook.com>
2026-07-23 17:00:24 -0300

    feat> padroniza erros

--- backend/src/middlewares/ErrorHandler.ts
+++ backend/src/middlewares/ErrorHandler.ts
@@ -1,20 +1,38 @@
 import { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
+import { AppError } from "../errors/AppError";
 
-// Esse middleware vai formatar cada resposta de erro. Ao invés de cada controller ter que pegar um erro e formatar a mensagem bonitinha, ele faz isso para todo mundo, tipo aquele seu amigo quie fez todo o trabalhp enquanto tu ficou no celular pq vcf sabia que ele ia fazer para ti msm.
-export function errorHandler(error: any, req: Request, res: Response, next:NextFunction){
-    // Antes de mais nada, a gent mostra o erro "na forma original" dele pra debugar
-     
-console.log("Erro capturado pelo erroHandler: ", error)
+export function errorHandler(error: Error, _req: Request, res: Response, _next: NextFunction) {
 
-// Esse tal de 'ER_DUP_ENTRY' é específico do MySQL: ele acontece quando a gente tenta salvar algo que já existe e tem UNIQUE (exemplo: criar um usuário com um email que já existe)
-if(error.code === 'ER_DUP_ENTRY'){
-    return res.status(409).json({
-        message: "Registro duplicado (email já existente)."
-    })
-}
+    if (error instanceof AppError) {
+        return res.status(error.statusCode).json({
+            success: false,
+            message: error.message
+        });
+    }
 
-// Se for quanquer outro erro que a gente não previu pq nn tem bola de cristal, ele vira um 500 genérico
-return res.status(500).json({
-    message: "Erro interno no servidor. Traduzindo: DEU RUIM, GURIZADA!!!!!!"
-})
+    if (error instanceof ZodError) {
+        const errors: Record<string, string[]> = {};
+
+        error.issues.forEach(issue => {
+            const field = issue.path.join('.');
+            if (!errors[field]) {
+                errors[field] = [];
+            }
+            errors[field].push(issue.message);
+        });
+
+        return res.status(400).json({
+            success: false,
+            message: 'Erro de Validação',
+            errors
+        });
+    }
+
+    console.error(error);
+
+    return res.status(500).json({
+        success: false,
+        message: "Internal Server Error"
+    });
 }
