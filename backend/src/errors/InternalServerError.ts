d534b11e0f5aca326e85462f8c05b072732e645c
Eduarda C. <dossantosec@outlook.com>
2026-07-23 17:00:24 -0300

    feat: padroniza erros

---
+++ backend/src/errors/InternalServerError.ts
@@ -0,0 +1,6 @@
+import { AppError } from "./AppError";
+export class InternalServerError extends AppError {
+    constructor(message = 'Internal Server Error') {
+        super(message, 500)
+    }
+}
