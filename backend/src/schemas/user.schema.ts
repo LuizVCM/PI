6957cbb13ae49c50f4bea9675e62230baa1fd3bd
Eduarda C. <dossantosec@outlook.com>
2026-07-30 16:50:16 -0300

    feat: melhora schema do user

--- backend/src/schemas/user.schema.ts
+++ backend/src/schemas/user.schema.ts
@@ -11,9 +11,17 @@ export const createUserSchema = z.object({
     .regex(/^(?=.*[A-Z])/, "A senha deve ter pelo menos uma letra maiúscula")
     .regex(/^(?=.*[a-z])/, "A senha deve ter pelo menos uma letra minúscula")
     .regex(/^(?=.*[0-9])/, "A senha deve ter pelo menos um dígito (0-9)")
-    .regex(/^(?=.*[@%!&*_])/, "A senha deve ter pelo menos um caractere especial (@, %, !. &, * ou _)")
+    .regex(
+      /^(?=.*[@%!&*_])/,
+      "A senha deve ter pelo menos um caractere especial (@, %, !. &, * ou _)"
+    )
     .max(255),
 });
 export const updateUserSchema = createUserSchema.partial();
+export const loginUserSchema = z.object({
+  email: z.email(),
+  senha: z.string().min(1, "A senha é obrigatória"),
+});
 export type CreateUserDTO = z.infer<typeof createUserSchema>;
 export type UpdateUserDTO = z.infer<typeof updateUserSchema>;
+export type LoginUserDTO = z.infer<typeof loginUserSchema>;
