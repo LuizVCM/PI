80cb37b6f4ff8c9b09002cf6b4abe9298f96baeb
LUIZ VINICIUS CASSABONE MUNIZ <04527409085@senacrs.edu.br>
2026-08-04 16:52:24 -0300

    feat: adiciona parte de gestão

---
+++ Pi-Projeto_Integrador/frontend/assets/js/pertguntasFrequente.js
@@ -0,0 +1,4 @@
+const td = document.querySelector(".content");
+const perguntasButton = document.querySelector(".perguntasFrequentes");
+const contato = document.querySelector(".contato");
+const perguntas = document.querySelector(".perguntas")
