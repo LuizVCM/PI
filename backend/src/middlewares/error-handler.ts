70e3f6ce479e52343f62e0dcec51d71a2fcadf6a
Eduarda C. <dossantosec@outlook.com>
2026-08-05 01:05:15 -0300

    chore: renomeia para padronizar

---
+++ backend/src/middlewares/error-handler.ts
@@ -0,0 +1,24 @@
+import { NextFunction, Request, Response } from "express";
+import { AppError } from "../errors/AppError";
+import { BadRequestError } from "../errors/BadRequestError";
+
+export function errorHandler(
+  error: Error,
+  _req: Request,
+  res: Response,
+  _next: NextFunction
+) {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      success: false,
+      message: error.message, ...(error instanceof BadRequestError && error.details ? { errors: error.details } : {}),
+    });
+  }
+
+  console.error(error);
+
+  return res.status(500).json({
+    success: false,
+    message: "Internal Server Error",
+  });
+}
--- backend/src/routes/user.routes.ts
+++ backend/src/routes/user.routes.ts
@@ -1,16 +1,15 @@
-import { validateUserUpdate } from '../middlewares/validateUSer';
+import { validateUserUpdate } from '../middlewares/validateUser';
 import { Router } from "express";
-import { UserController } from "../controllers/UserControllers";
-import { validateUserCreate } from "../middlewares/validateUSer";
+import { UserController } from "../controllers/UserController";
+import { validateUserCreate } from "../middlewares/validateUser";
 
-const router = Router() 
+const userRoutes = Router() 
 const userController = new UserController()
 
-router.get('/', userController.list.bind(userController))
-router.get('/:id', userController.getById.bind(userController))
-router.post('/', validateUserCreate, userController.create.bind(userController))
-router.put('/:id',  validateUserUpdate, userController.update.bind(userController))
-router.delete('/:id', userController.delete.bind(userController))
+userRoutes.get('/', userController.list.bind(userController))
+userRoutes.get('/:id', userController.getById.bind(userController))
+userRoutes.post('/', validateUserCreate, userController.create.bind(userController))
+userRoutes.put('/:id',  validateUserUpdate, userController.update.bind(userController))
+userRoutes.delete('/:id', userController.delete.bind(userController))
 
-
-export default router
+export default userRoutes
