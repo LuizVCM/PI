260c806ab92289b3e727dc1aa3534ce728ff7874
Eduarda C. <dossantosec@outlook.com>
2026-08-10 02:00:57 -0300

    feat: implementa autenticação por cookie

--- backend/src/routes/crop.routes.ts
+++ backend/src/routes/crop.routes.ts
@@ -1,15 +1,31 @@
 import { Router } from "express";
 import { CropController } from "../controllers/CropController";
-// import { authMiddleware } from "../middlewares/authMiddleware";
+import { AuthMiddleware } from "../middlewares/auth-middleware";
 
 const cropRoutes = Router();
 const cropController = new CropController();
 
 cropRoutes.get("/", cropController.list);
-cropRoutes.get("/me", /* authMiddleware ,*/ cropController.listMyPosts.bind(cropController));
+cropRoutes.get(
+  "/me",
+  AuthMiddleware,
+  cropController.listMyCrops.bind(cropController)
+);
 cropRoutes.get("/:id", cropController.getById.bind(cropController));
-cropRoutes.post("/", /* authMiddleware ,*/ cropController.create.bind(cropController));
-cropRoutes.put("/:id", /* authMiddleware ,*/ cropController.update.bind(cropController));
-cropRoutes.delete("/:id", /* authMiddleware ,*/ cropController.delete.bind(cropController));
+cropRoutes.post(
+  "/",
+  AuthMiddleware,
+  cropController.create.bind(cropController)
+);
+cropRoutes.put(
+  "/:id",
+  AuthMiddleware,
+  cropController.update.bind(cropController)
+);
+cropRoutes.delete(
+  "/:id",
+  AuthMiddleware,
+  cropController.delete.bind(cropController)
+);
 
 export default cropRoutes;
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import userRoutes from "./user.routes";
 import cropRoutes from "./crop.routes";
+import authRoutes from "./auth.routes";
 const router = Router();
 router.use("/users", userRoutes);
 router.use("/crops", cropRoutes);
+router.use("/auth", authRoutes); // login, logout e checar senha
 export default router;
