08a82a4eaa91b5cb2f4f1c2f1fd9a50d80aae922
Eduarda C. <dossantosec@outlook.com>
2026-08-10 15:52:17 -0300

    feat: adiciona mapper

---
+++ backend/src/utils/UserMapper.ts
@@ -0,0 +1,24 @@
+import { User } from "../models/User";
+
+export class UserMapper {
+    static toResponseCreated(user: User) {
+        return {
+            nome: user.nome,
+            sobrenome: user.sobrenome,
+            email: user.email,
+            telefone: user.telefone,
+            cpf: user.cpf
+        }
+    }
+    static toResponse(user: User) {
+        return {
+            nome: user.nome,
+            sobrenome: user.sobrenome,
+            email: user.email,
+            territorios: user.territorios,
+            sementes: user.sementes,
+            financas: user.financas,
+            insumos: user.insumos
+        }
+    } 
+}
