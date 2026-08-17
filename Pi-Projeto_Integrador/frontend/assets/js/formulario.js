c06e1c7313c75b9c24c614a7e7fceef3e84540a6
LUIZ VINICIUS CASSABONE MUNIZ <04527409085@senacrs.edu.br>
2026-08-17 14:44:02 -0300

    feat: sla, resolve algo

--- Pi-Projeto_Integrador/frontend/assets/js/formulario.js
+++ Pi-Projeto_Integrador/frontend/assets/js/formulario.js
@@ -1,7 +1,17 @@
 const formulario = document.querySelectorAll(".fundo");
 const click = document.getElementById("paragrafoLink");
 const click2 = document.getElementById("entra2");
+const click3 = document.getElementById("entra3");
+const click4 = document.getElementById("entra4")
 const entrar = document.getElementById("find");
+const home = document.querySelector('.entrar')
+
+const cadastroPag = document.querySelector("cadastro")
+const cadastroUserBtn = document.getElementById("cadastrar")
+const territorio = document.querySelector(".territorio")
+const sensor = document.querySelector(".sensor")
+const territorioBtn = document.getElementById("territorio");
+const sensorBtn = document.getElementById('sensor')
 
 click.addEventListener("click", () => {
     formulario.forEach(element => {
@@ -13,4 +23,15 @@ click2.addEventListener("click", () => {
         element.classList.toggle("cadastro");
     });
 });
+
+
+
+cadastroUserBtn.addEventListener("click", () => {
+       territorio.classList.toggle("territorio")
+       cadastroPag.classList.toggle("cadastro")
+
+})
+
+
+
 
