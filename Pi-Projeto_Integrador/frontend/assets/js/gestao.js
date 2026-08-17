9528c858ad2646613c116f781a03828c54ade817
Erica Barbosa <erica101barbosa@gmail.com>
2026-08-17 14:29:21 -0300

    Ajusta responsividade das telas de gestão

--- Pi-Projeto_Integrador/frontend/assets/js/gestao.js
+++ Pi-Projeto_Integrador/frontend/assets/js/gestao.js
@@ -1,117 +1,138 @@
+// =============================
+// TELAS
+// =============================
 
-
-// Pegando as telas pelo ID do HTML
-// O JavaScript precisa encontrar essas partes para conseguir trocar entre elas
-
-const telaHome = document.getElementById("tela-home");
-
+const telaGestao = document.getElementById("tela-gestao");
 const telaCadastro = document.getElementById("tela-cadastro");
-
 const telaAgenda = document.getElementById("tela-agenda");
-
 const telaInsumos = document.getElementById("tela-insumos");
-
 const telaEstoque = document.getElementById("tela-estoque");
 
 
-// ================= BOTÕES =================
-
-// Pegando os botões pelo ID para conseguir colocar ações neles
+// =============================
+// BOTÕES
+// =============================
 
 const btnCadastro = document.getElementById("btnCadastro");
-
 const btnVoltar = document.getElementById("btnVoltar");
 
 const btnAgenda = document.getElementById("btnAgenda");
-
 const btnVoltarAgenda = document.getElementById("btnVoltarAgenda");
 
 const btnInsumos = document.getElementById("btnInsumos");
-
 const btnVoltarInsumos = document.getElementById("btnVoltarInsumos");
 
 const btnEstoque = document.getElementById("btnEstoque");
-
 const btnVoltarEstoque = document.getElementById("btnVoltarEstoque");
 
-// Quando clicar no botão cadastro
+
+// =============================
+// CADASTRO
+// =============================
+
 btnCadastro.addEventListener("click", () => {
-    // Esconde a tela inicial
-    telaHome.classList.remove("ativa")
-    telaHome.classList.add("hidden");
-    // Mostra a tela de cadastro
+
+    telaGestao.classList.remove("ativa");
+    telaGestao.classList.add("hidden");
+
     telaCadastro.classList.remove("hidden");
     telaCadastro.classList.add("ativa");
+
 });
 
-// Quando clicar no botão voltar do cadastro
+
+// VOLTAR DO CADASTRO
+
 btnVoltar.addEventListener("click", () => {
-    // Esconde a tela de cadastro
+
     telaCadastro.classList.remove("ativa");
     telaCadastro.classList.add("hidden");
-    // Mostra novamente a tela inicial
-    telaHome.classList.remove("hidden");
-    telaHome.classList.add("ativa");
+
+    telaGestao.classList.remove("hidden");
+    telaGestao.classList.add("ativa");
+
 });
 
-// Quando clicar no botão agenda
+
+// =============================
+// AGENDA
+// =============================
+
 btnAgenda.addEventListener("click", () => {
-    // Esconde a tela inicial
-    telaHome.classList.remove("ativa");
-    telaHome.classList.add("hidden");
-    // Mostra a tela de agenda
+
+    telaGestao.classList.remove("ativa");
+    telaGestao.classList.add("hidden");
+
     telaAgenda.classList.remove("hidden");
     telaAgenda.classList.add("ativa");
+
 });
 
-// Quando clicar no botão voltar da agenda
+
+// VOLTAR DA AGENDA
+
 btnVoltarAgenda.addEventListener("click", () => {
-    // Esconde a tela agenda
+
     telaAgenda.classList.remove("ativa");
     telaAgenda.classList.add("hidden");
-    // Mostra a tela inicial novamente
-    telaHome.classList.remove("hidden");
-    telaHome.classList.add("ativa");
+
+    telaGestao.classList.remove("hidden");
+    telaGestao.classList.add("ativa");
+
 });
 
 
+// =============================
+// INSUMOS
+// =============================
 
-// Abrir tela de insumos
 btnInsumos.addEventListener("click", () => {
-    telaHome.classList.remove("ativa");
-    telaHome.classList.add("hidden");
+
+    telaGestao.classList.remove("ativa");
+    telaGestao.classList.add("hidden");
 
     telaInsumos.classList.remove("hidden");
     telaInsumos.classList.add("ativa");
+
 });
 
-// Voltar para a tela inicial
+
+// VOLTAR DOS INSUMOS
+
 btnVoltarInsumos.addEventListener("click", () => {
+
     telaInsumos.classList.remove("ativa");
     telaInsumos.classList.add("hidden");
 
-    telaHome.classList.remove("hidden");
-    telaHome.classList.add("ativa");
+    telaGestao.classList.remove("hidden");
+    telaGestao.classList.add("ativa");
+
 });
 
-// Abrir tela
+
+// =============================
+// ESTOQUE
+// =============================
+
 btnEstoque.addEventListener("click", () => {
 
-    telaHome.classList.remove("ativa");
-    telaHome.classList.add("hidden");
+    telaGestao.classList.remove("ativa");
+    telaGestao.classList.add("hidden");
 
     telaEstoque.classList.remove("hidden");
     telaEstoque.classList.add("ativa");
 
 });
 
-// Voltar
+
+// VOLTAR DO ESTOQUE
+
 btnVoltarEstoque.addEventListener("click", () => {
 
     telaEstoque.classList.remove("ativa");
     telaEstoque.classList.add("hidden");
 
-    telaHome.classList.remove("hidden");
-    telaHome.classList.add("ativa");
+    telaGestao.classList.remove("hidden");
+    telaGestao.classList.add("ativa");
 
 });
