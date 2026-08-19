37d35bf300f98ee0e9e8cf2b356ef145a87971cb
Eduarda C. <dossantosec@outlook.com>
2026-08-19 16:37:24 -0300

    refactor: adapta finance model

--- backend/src/models/Finance.ts
+++ backend/src/models/Finance.ts
@@ -1,28 +1,24 @@
-import {
-  Column,
-  Entity,
-  ManyToOne,
-} from "typeorm";
+import { Column, Entity, ManyToOne } from "typeorm";
 import { User } from "./User";
 import { BaseModel } from "./BaseModel";
 
+export enum FinanceType {
+  DESPESA = "despesa",
+  LUCRO = "lucro"
+}
+
 @Entity("financas")
 export class Finance extends BaseModel {
+  @Column({ type: "enum", enum: FinanceType, nullable: false })
+  tipo: FinanceType;
   @Column({ type: "double", nullable: false })
   valor: number;
-
-  @Column({ type: "enum", enum: ["gasto", "ganho"], nullable: false })
-  tipo: "gasto" | "ganho";
-
-  @Column({ type: "double", nullable: false })
-  quantidade: number;
-
   @Column({ type: "text", nullable: false })
-  descricao: string;
-
+  observacoes: string;
+  @Column({ type: "text", nullable: false })
+  detalhes: string;
   @Column({ name: "data_criacao", type: "date", nullable: false })
   data: Date;
-
   @ManyToOne(() => User, (usuario) => usuario.financas)
   usuario: User;
 }
