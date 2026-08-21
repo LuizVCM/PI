c256f6a8fffae3f467a49d725fd5055c9c7f5ce8
Eduarda C. <dossantosec@outlook.com>
2026-08-21 14:20:29 -0300

    refactor: melhora stock model

--- backend/src/models/Stock.ts
+++ backend/src/models/Stock.ts
@@ -2,14 +2,19 @@ import { Column, Entity, ManyToOne } from "typeorm";
 import { User } from "./User";
 import { BaseModel } from "./BaseModel";
 
+export enum WeightUnit {
+  L = "l",
+  KG = "kg"
+}
+
 @Entity("estoque_insumos")
 export class Stock extends BaseModel {
-  @Column({ name: "nome_insumo" })
-  nomeInsumo: string;
+  @Column({ length: 100 })
+  nome: string;
   @Column({ type: "decimal", scale: 2, precision: 5, nullable: false })
   quantidade: number;
-  @Column({ type: "enum", enum: ["litros", "quilogramas"], nullable: false })
-  unidade: "litros" | "quilogramas";
+  @Column({ type: "enum", enum: WeightUnit, nullable: false })
+  unidade: WeightUnit;
   @Column({ name: "data_validade", type: "date" })
   dataValidade: Date;
   @ManyToOne(() => User, (usuario) => usuario.insumos)
