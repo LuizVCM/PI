c00ffd1050aec7071514bd37704ebcb2ac49019d
Eduarda C. <dossantosec@outlook.com>
2026-08-21 01:19:38 -0300

    refactor: altera tipo do campo valor para decimal

--- backend/src/models/Finance.ts
+++ backend/src/models/Finance.ts
@@ -4,20 +4,25 @@ import { BaseModel } from "./BaseModel";
 
 export enum FinanceType {
   DESPESA = "despesa",
-  LUCRO = "lucro"
+  LUCRO = "lucro",
 }
 
 @Entity("financas")
 export class Finance extends BaseModel {
   @Column({ type: "enum", enum: FinanceType, nullable: false })
   tipo: FinanceType;
-  @Column({ type: "double", nullable: false })
+  @Column({
+    type: "decimal",
+    precision: 10,
+    scale: 2,
+    nullable: false,
+  })
   valor: number;
   @Column({ type: "text", nullable: true })
   observacoes: string | null;
   @Column({ type: "text", nullable: true })
   detalhes: string | null;
-  @Column({ name: "data_criacao", type: "date", nullable: false })
+  @Column({ name: "data", type: "date", nullable: false })
   data: Date;
   @ManyToOne(() => User, (usuario) => usuario.financas)
   usuario: User;
