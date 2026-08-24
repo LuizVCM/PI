10e78cba1ad18a10ea45a96cb642bb0c5b34e7f2
Eduarda C. <dossantosec@outlook.com>
2026-08-24 01:35:56 -0300

    feat: adiciona dados de plantas no inicio do server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,18 +4,25 @@ import { AppDataSource } from "./config/data-source";
 import router from "./routes/index.routes";
 import { errorHandler } from "./middlewares/error-handler";
 import cookieParser from "cookie-parser";
+import { insertPlants } from "./config/data/plant.data";
 const app: Application = express();
 dotenv.config();
 app.use(cookieParser());
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(router);
-AppDataSource.initialize()
-  .then(() => {
+async function startServer() {
+  try {
+    await AppDataSource.initialize();
     console.log("Banco conectado com sucesso");
+    await insertPlants();
+    console.log("Dados inseridos com sucesso");
     app.use(errorHandler);
     app.listen(PORT, () => {
       console.log(`Servidor rodando na porta ${PORT}`);
     });
-  })
-  .catch((error) => console.log("Erro ao conectar com o banco: " + error));
+  } catch (error) {
+    console.error("Erro ao iniciar aplicação: ", error);
+  }
+}
+startServer();
