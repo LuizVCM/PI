8166dd620ff710780d55491ff5cedf700861f79b
Eduarda C. <dossantosec@outlook.com>
2026-09-02 01:34:43 -0300

    refactor: adapta schemas

--- backend/src/schemas/stock.schema.ts
+++ backend/src/schemas/stock.schema.ts
@@ -1,9 +1,9 @@
 import z from "zod";
-import { MeasurementUnit } from "../models/Stock";
+import { StockUnit } from "../models/Stock";
 export const createStockSchema = z.object({
   nome: z.string().trim().min(1, "No mínimo 1 caracter").max(100, "No máximo 100 caracteres"),
   quantidade: z.coerce.number("A quantidade deve ser um número").positive("A quantidade deve ser positiva"),
-  unidade: z.enum(MeasurementUnit, "Tipo de unidade inválido"),
+  unidade: z.enum(StockUnit, "Tipo de unidade inválido"),
   dataValidade: z.coerce.date("Formato de data inválido"),
 });
 export const updateStockSchema = createStockSchema.partial();
