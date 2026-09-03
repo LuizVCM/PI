316b7349aca1189bd3b59703baf44785e7c3f102
Eduarda C. <dossantosec@outlook.com>
2026-09-03 01:12:18 -0300

    chore: mescla alterações

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,6 @@ import { insertPlants } from "./config/seed/plant.seeder";
 const app: Application = express();
 dotenv.config();
 const PORT = process.env.PORT;
-
 app.use(
   cors({
     origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
--- frontend/assets/js/pertguntasFrequente.js
+++
@@ -1,4 +0,0 @@
-const td = document.querySelector(".content");
-const perguntasButton = document.querySelector(".perguntasFrequentes");
-const contato = document.querySelector(".contato");
-const perguntas = document.querySelector(".perguntas")
