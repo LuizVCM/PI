bd8b5c1c03c64f7391a25634ec22cd1252aea4be
Eduarda C. <dossantosec@outlook.com>
2026-09-03 14:02:53 -0300

    refactor: atualiza data sensor

--- backend/src/schemas/data-sensor.schema.ts
+++ backend/src/schemas/data-sensor.schema.ts
@@ -1,6 +1,5 @@
 import z from "zod";
 export const createDataSensor = z.object({
   valor: z.number(),
-  unidade: z.enum(["porcento", "celsius", "kmh"])
 });
 export type CreateDataSensorDTO = z.infer<typeof createDataSensor>;
