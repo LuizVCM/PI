15faadb6cda08930265d7cb7099a3e6d8312310b
Eduarda C. <dossantosec@outlook.com>
2026-09-08 16:52:13 -0300

    refactor: adapta controllers

--- backend/src/controllers/SensorController.ts
+++ backend/src/controllers/SensorController.ts
@@ -1,14 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 import { SensorService } from "../services/SensorService";
-import {
-  CreateSensorDTO,
-  UpdateSensorDTO,
-} from "../schemas/sensor.schema";
+import { CreateSensorDTO, UpdateSensorDTO } from "../schemas/sensor.schema";
 import { TerritoryService } from "../services/TerritoryService";
 
 export class SensorController {
   private sensorService = new SensorService();
-  private territoryService = new TerritoryService();
 
   async list(req: Request, res: Response, next: NextFunction) {
     try {
@@ -43,10 +39,13 @@ export class SensorController {
   async create(req: Request, res: Response, next: NextFunction) {
     try {
       const loggedUser = req.user!.id;
-      await this.territoryService.listByUserLogged(loggedUser);
       const territoryId = Number(req.params.id);
       const createSensorData = req.body as CreateSensorDTO;
-      const sensor = await this.sensorService.create(createSensorData, territoryId);
+      const sensor = await this.sensorService.create(
+        createSensorData,
+        territoryId,
+        loggedUser
+      );
       return res.status(201).json(sensor);
     } catch (error) {
       next(error);
