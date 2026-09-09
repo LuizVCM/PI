6a4667633e80d34daae66eb2482b87907d15a79b
Eduarda C. <dossantosec@outlook.com>
2026-09-09 01:42:02 -0300

    refactor: padroniza controllers

--- backend/src/controllers/AuthController.ts
+++ backend/src/controllers/AuthController.ts
@@ -1,5 +1,5 @@
 import { LoginUserDTO } from "./../schemas/user.schema";
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { UserService } from "../services/UserService";
 import { AuthService } from "../services/AuthService";
 import { UnauthorizedError } from "../errors/UnauthorizedError";
@@ -7,49 +7,59 @@ import { UnauthorizedError } from "../errors/UnauthorizedError";
 export class AuthController {
   private authService: AuthService = new AuthService();
   private userService: UserService = new UserService();
+  async login(req: Request, res: Response, next: NextFunction) {
+    try {
+      const loginData = req.body as LoginUserDTO;
+      const loggedUser = await this.userService.login(loginData);
+      const token = this.authService.generate({
+        id: loggedUser.id,
+        email: loggedUser.email,
+        role: loggedUser.role,
+      });
 
-  async login(req: Request, res: Response) {
-    const loginData = req.body as LoginUserDTO;
-    const loggedUser = await this.userService.login(loginData);
-    const token = this.authService.generate({
-      id: loggedUser.id,
-      email: loggedUser.email,
-      role: loggedUser.role
-    });
+      // console.log("Token:", token);
 
-    // console.log("Token:", token);
+      res.cookie("token", token, {
+        httpOnly: true,
+        secure: true, // false -> thunderclient, true -> front
+        sameSite: "none", // 'lax' -> thunderclient, 'none' -> front
+        maxAge: 1000 * 60 * 60, // 1h
+      });
 
-    res.cookie("token", token, {
-      httpOnly: true,
-      secure: true, // false -> thunderclient, true -> front
-      sameSite: 'none', // 'lax' -> thunderclient, 'none' -> front
-      maxAge: 1000 * 60 * 60, // 1h
-    });
+      // console.log("Headers:", res.getHeaders());
 
-    // console.log("Headers:", res.getHeaders());
-
-    return res.status(200).json({
-      success: true,
-    });
+      return res.status(200).json({
+        success: true,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
-
-  async logout(req: Request, res: Response) {
-    res.clearCookie("token");
-    return res.sendStatus(204);
+  async logout(req: Request, res: Response, next: NextFunction) {
+    try {
+      res.clearCookie("token");
+      return res.sendStatus(204);
+    } catch (error) {
+      next(error);
+    }
   }
   /** para solicitar confirmação ao alterar dados sensíveis, se necessário  */
-  async checkUserPassword(req: Request, res: Response) {
-    const { senha } = req.body;
-    if (!req.user?.email) {
-      throw new UnauthorizedError("não autenticado");
+  async checkUserPassword(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { senha } = req.body;
+      if (!req.user?.email) {
+        throw new UnauthorizedError("não autenticado");
+      }
+      const passwordIsValid = await this.userService.checkUserPassword(
+        req.user?.email,
+        senha
+      );
+      if (!passwordIsValid) {
+        throw new UnauthorizedError("credenciais inválidas");
+      }
+      return res.status(200).json({ success: true });
+    } catch (error) {
+      next(error);
     }
-    const passwordIsValid = await this.userService.checkUserPassword(
-      req.user?.email,
-      senha
-    );
-    if (!passwordIsValid) {
-      throw new UnauthorizedError("credenciais inválidas");
-    }
-    return res.status(200).json({ success: true });
   }
 }
