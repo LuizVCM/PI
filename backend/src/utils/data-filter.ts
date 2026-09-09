3df5461b5def5f21a5cd73f3b57114aca7944cbe
LuizVCM <luizvcmluizvcm@gmail.com>
2026-09-09 13:55:37 -0300

    feat: adiciona frontend

--- backend/src/utils/data-filter.ts
+++
@@ -1,8 +0,0 @@
-export function dataFilter(entity: Object, data: Object) {
-  return Object.assign(
-    entity,
-    Object.fromEntries(
-      Object.entries(data).filter(([, value]) => value !== undefined)
-    )
-  );
-}
