7ed60d2f28bbbaaffb61c168dae9816322e4b89e
Eduarda C. <dossantosec@outlook.com>
2026-09-09 01:43:11 -0300

    feat: atualiza mapper e schema data-sensor

--- backend/src/middlewares/index.validate.ts
+++ backend/src/middlewares/index.validate.ts
@@ -1,11 +1,14 @@
 import { createCropSchema, updateCropSchema } from "../schemas/crop.schema";
+import { createDataSensorSchema } from "../schemas/data-sensor.schema";
 import {
   createFinanceSchema,
   updateFinanceSchema,
 } from "../schemas/finance.schema";
-import { createPlantSchema, updatePlantSchema } from "../schemas/plant.schema";
 import { createSeedSchema, updateSeedSchema } from "../schemas/seed.schema";
-import { createSensorSchema, updateSensorSchema } from "../schemas/sensor.schema";
+import {
+  createSensorSchema,
+  updateSensorSchema,
+} from "../schemas/sensor.schema";
 import { createStockSchema, updateStockSchema } from "../schemas/stock.schema";
 import {
   createTerritorySchema,
@@ -31,10 +34,9 @@ export const validateFinanceCreate = validate(createFinanceSchema);
 export const validateFinanceUpdate = validate(updateFinanceSchema);
 export const validateStockCreate = validate(createStockSchema);
 export const validateStockUpdate = validate(updateStockSchema);
-export const validatePlantCreate = validate(createPlantSchema);
-export const validatePlantUpdate = validate(updatePlantSchema);
 export const validateSeedCreate = validate(createSeedSchema);
 export const validateSeedUpdate = validate(updateSeedSchema);
 export const validateWeatherCreate = validate(createWeatherSchema);
 export const validateSensorCreate = validate(createSensorSchema);
 export const validateSensorUpdate = validate(updateSensorSchema);
+export const validateDataSensorCreate = validate(createDataSensorSchema);
