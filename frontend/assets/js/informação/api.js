0f36815c54448c8de556be78900c976308af143d
LuizVCM <luizvcmluizvcm@gmail.com>
2026-09-16 16:05:55 -0300

    feat: continua produção

--- frontend/assets/js/informação/api.js
+++ frontend/assets/js/informação/api.js
@@ -1,5 +1,12 @@
 import { formatarData, escapeHtml, capitalizar } from "../utils/formatter.js";
+import { openMateo } from "../Cálculos.js";
 const select = document.getElementById("sementeInfo")
+const fonecedor = document.querySelector(".loteIdent div");
+const nome = document.getElementById("nomePlanta");
+
+const expecativa = document.getElementById("expectativaVida");
+
+const observacoes = document.querySelector(".cartao ul li")
 
 const plantasAPI = 'http://localhost:3000/plants/me';
 async function trazerPlantas() {
@@ -16,10 +23,35 @@ async function trazerPlantas() {
       `<option value="${p.id}">${escapeHtml(p.nome)}</option>`
     );
   });
+function valor(){
+   const textoSelecionado = select.options[select.selectedIndex].text;
+   console.log(plantas)
+
+nome.textContent = `nome: ${textoSelecionado}`
+
+const plantaEncontrada = plantas.find(
+  (planta) => planta.nome.toLowerCase() === textoSelecionado.toLowerCase()
+);
+
+console.log(plantaEncontrada)
+fonecedor.textContent = `fornecedor: ${plantaEncontrada.sementes[0].fornecedor}`
+expecativa.textContent = `de ${plantaEncontrada.cicloMinimoDias} a ${plantaEncontrada.cicloMaximoDias} dias`
+const chavePermanente = nome.textContent 
+
+ localStorage.setItem(chavePermanente, plantaEncontrada.sementes[0].fornecedor);
+
+ observacoes.textContent = `${plantaEncontrada.sementes[0].observacoes}`
+}
+const botao = document.querySelector(".acessando");
+botao.addEventListener("click", valor)
+
+// cálculo 
+openMateo()
+
     }catch(error){
 console.log("deu errado na requisição de plantas: "+ error)
     }
 }
 trazerPlantas()
-setInterval(trazerPlantas, 1000000)
+setInterval(trazerPlantas, 100000)
 
