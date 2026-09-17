a394571950f202a8c3240eb2437230e2e0020078
Eduarda C. <dossantosec@outlook.com>
2026-09-17 14:37:30 -0300

    refactor: ajusta schema

--- backend/src/schemas/stock.schema.ts
+++ backend/src/schemas/stock.schema.ts
@@ -1,11 +1,12 @@
 import z from "zod";
-import { StockUnit } from "../models/Stock";
+import { StockCategory, StockUnit } from "../models/Stock";
 export const createStockSchema = z.object({
   nome: z
     .string()
     .trim()
     .min(1, "No mínimo 1 caracter")
     .max(100, "No máximo 100 caracteres"),
+  categoria: z.enum(StockCategory, "Tipo de categoria inválido"),
   quantidade: z.coerce
     .number("A quantidade deve ser um número")
     .positive("A quantidade deve ser positiva"),
