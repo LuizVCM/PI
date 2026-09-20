5abb3141cc33dc6e3a5a534042954fdf795f2c8d
Eduarda C. <dossantosec@outlook.com>
2026-09-20 00:55:46 -0300

    feat: redesenha página inicial com cards de acesso rápido

--- frontend/assets/js/home.js
+++ frontend/assets/js/home.js
@@ -1,8 +1,8 @@
-import { apiFetch } from "./config/api.js";
+import { carregarUsuario } from "./utils/load-user-data.js"
 
 const rotas = {
   clima: "Clima.html",
-  gestao: "Gestao.html",
+  gestao: "gestao.html",
   segurancas: "Seguranca.html",
   sementes: "sementes.html",
   financas: "Financas.html",
@@ -26,22 +26,7 @@ menuToggle.addEventListener("click", () => {
   aside.classList.toggle("aside-encolhido");
 });
 
-export async function carregarUsuario() {
-  try {
-    const usuario = await apiFetch("/users/me", { method: "GET" });
-    document.getElementById(
-      "welcome-message"
-    ).textContent = `Olá, ${usuario.nome}!`;
-    document.getElementById(
-      "user-abbrev"
-    ).textContent = `${usuario.nome.substring(0, 2)}`;
-    return usuario;
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-carregarUsuario();
+await carregarUsuario();
 
 // menu mobile
 
