798d4267d8d86037e336f060858c968161242233
Eduarda C. <dossantosec@outlook.com>
2026-09-20 20:37:03 -0300

    refactor: atualiza páginas para nova estrutura
    também atualiza barra lateral para ter botão "início"

--- frontend/assets/js/utils/change-content.js
+++
@@ -1,4 +0,0 @@
-export function mostrarConteudo(id) {
-  document.querySelectorAll(".content").forEach((el) => el.classList.add("hidden"));
-  document.getElementById(id)?.classList.remove("hidden");
-}
